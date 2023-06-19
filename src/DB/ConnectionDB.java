8d7181d634db67799ef1cd342ebe723b672662af
Pedro <pedrodias3737@gmail.com>
2023-06-19 11:43:01 -0300

    DB package addition

---
+++ src/DB/ConnectionDB.java
@@ -0,0 +1,72 @@
+package DB;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+public class ConnectionDB {
+	
+	private static Connection conn = null;
+	
+	public static Connection getConnection(){
+		if(conn == null) {
+			try{
+				Properties props = loadProperties();
+				String url = props.getProperty("dburl");
+				conn = DriverManager.getConnection(url, props);
+				return conn;
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+		return conn;
+	}
+	
+	public static void closeConnection() {
+		if(conn != null) {
+			try {
+				conn.close();
+			}catch(SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+			
+		}
+	}
+	
+	public static void closeStatement(Statement st) {
+		if(st != null) {
+			try {
+				st.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	public static void closeResultSet(ResultSet rs) {
+		if(rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new DbException(e.getMessage());
+			}
+		}
+	}
+	
+	
+	private static Properties loadProperties() {
+		try(FileInputStream fs = new FileInputStream("db.properties")){
+			Properties properties = new Properties();
+			properties.load(fs);
+			return properties;
+		} catch (IOException e) {
+			throw new DbException(e.getMessage());
+		}
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author pedro
+ *
+ */
+module ProjectDAOJDBC {
+	requires java.sql;
+}
